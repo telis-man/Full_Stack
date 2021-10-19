739f951cd77eaede2bb811a1afc621622453fe2a
Mantas <nacevicius@gmail.com>
2021-10-19 18:09:11 +0300

    Masyvai

--- Class_10-18/script.js
+++ Class_10-18/script.js
@@ -8,6 +8,7 @@ let arrDoubled = input_array => input_array.map(item => item * 2);
 
 console.log(arrDoubled(numbers));
 
+
 // 2. Parašykite funkciją arrMultiplied, kuri sukuria ir grąžina naują masyvą, kurio elementai padauginti iš argumentu nurodyto skaičiaus
 
 console.log(`2 užduotis:\n`)
@@ -16,6 +17,7 @@ let arrMultiplied = (input_array, multiplier) => input_array.map(item => item * 
 
 console.log(arrMultiplied(numbers, 5));
 
+
 // 3. Parašykite funkciją arrCountTwos, kuri suskaičiuoja dvejetus masyve
 
 console.log(`3 užduotis:\n`)
@@ -23,6 +25,7 @@ console.log(`3 užduotis:\n`)
 let arrCountTwos = (input_array) => input_array.filter(item => item == 2).length;
 
 console.log(arrCountTwos(numbers));
+
 
 // 4. Parašykite funkciją arrIndexOfFirst, kuri grąžintu pirmo surasto, argumentu nurodyto skaičiaus, indeksą masyve. Jei skaičius nerastas funkcija turi grąžinti -1.
 
@@ -39,6 +42,7 @@ console.log(`5 užduotis:\n`)
 let arrIndexOfLast = (input_array, argument) => input_array.lastIndexOf(argument);
 
 console.log(arrIndexOfLast(numbers, 2));
+
 
 // 6. Parašykite funkciją reverseNumbers, kuri pakeis skaičius vietomis taip, kad pirmas skaičius taps paskutiniu, antras piešpaskutiniu, o buvęs paskutinis taps pirmu, priešpaskutinis bus antru.
 // Pvz.: Turime skaičius 32243;
@@ -60,6 +64,7 @@ let findLowHigh = (input_array) => console.log(`Mažiausias: ${input_array.sort(
 
 findLowHigh(numbers);
 
+
 // 8. Parašykite  funkciją checkForLetters, kuri priims du argumentus: Pirmas argumentas bus sakinys (arba žodžiai (-is)) ir antras argumentas bus raidė (kaip string). Funkcija turės suskaičiuoti kiek pirmu agrumentu nurodytame sakinyje/žodžiuose(-yje) yra antru argumentu nurodytų raidžių ir gražinti tų raidžių sumą su sakiniu pvz.: “Raidė “v” sakinyje rasta 4 kartus”.
 
 console.log(`8 užduotis:\n`)
@@ -68,21 +73,25 @@ let checkForLetters = (sentence, letter) => sentence.split('').filter(item => it
 
 console.log(checkForLetters('Darkness imprisoning me all that I see absolute horror', 's'));
 
+
 // 9. Parašykite funckiją, kuri ras visus skaičius esančius msyve ir gražins juos kaip atsikrą masyvą. Naujame masyve visi skaičiai bus surikiuoti nuo mažiausio iki didžiausio.
 //   let arr2 = [8, 'Hello', 'click', 'u', 7, 4, 'a', 'a', 3, 6, "chair", ,10, 1];
 // Iškvietus funkciją rezultata bus: [1,3,4,6,7,8,10];
 
 console.log(`9 užduotis:\n`)
+
 let arr2 = [8, 'Hello', 'click', 'u', 7, 4, 'a', 'a', 3, 6, "chair", , 10, 1];
 let createSortedArray = (input_array) => input_array.filter(item => typeof(item) == 'number').sort((a, b) => a - b);
 
 console.log(createSortedArray(arr2));
+
 
 // 10. Sukurkite funkciję checkIfAllSmaller(arr, max), BE MASYVO METODŲ, kuri patikrintų ar visi skaičiai masyve yra didesni negu perduota reikšmė max;
 // Pvz.: Turime masyvą: let arr1 = [2, 7, 6, 9, 5];
 // Iškvietus funkciją checkIfAllSmaller(arr1, 5) rezultata bus: False
 
 console.log(`10 užduotis:\n`)
+
 let checkIfAllSmaller = (input_array, maximum_value) => {
 
     for (let i = 0; i < input_array.length; i++) {
@@ -94,11 +103,15 @@ let checkIfAllSmaller = (input_array, maximum_value) => {
 }
 
 let arr1 = [2, 7, 6, 9, 5];
+
 console.log(checkIfAllSmaller(arr1, 5));
+
 
 // 11. Parašykite funkciją filteredByLetter, kuri turi du parametrus: 1. masyvas; 2. raidė. Funkcija sukuria ir grąžina naują masyvą, kuriame yra visi masyvo, nurodyto kaip pirmas parametras elemntai, kuriuose galima rasti antru paramatetru nurodytą raidę.
 // Testuosime šį masyvą
+
 console.log(`11 užduotis:\n`)
+
 let citiesOfLithuania = [
     'Vilnius',
     'Kaunas',
@@ -126,6 +139,7 @@ let filteredByLetter = (input_array, letter) => {
 
 console.log(filteredByLetter(citiesOfLithuania, 'i'));
 
+
 // 12. Parašykite penkias funkcijas:
 // - calculateValue()
 // - addition()
@@ -140,6 +154,7 @@ console.log(filteredByLetter(citiesOfLithuania, 'i'));
 // Pastaba: šios funkcijos: addition(), subtraction(), multiplication(), division() turi būti kviečiamas calculateValue() viduje, o aprašomos išorėje.
 
 console.log(`12 užduotis:\n`)
+
 let addition = (number_1, number_2) => number_1 + number_2;
 let subtraction = (number_1, number_2) => number_1 - number_2;
 let multiplication = (number_1, number_2) => number_1 * number_2;
